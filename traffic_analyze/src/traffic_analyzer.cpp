46c8adb10d86a70a994e7792a27372da654b8028
jkim838 <jkim838@aucklanduni.ac.nz>
2019-07-11 22:05:17 +1200

    *Fixed the issue where not all bounding boxes will be recorded in CSV file *Implemented a feature to record trajectory in Traffic Tracker node

--- traffic_analyze/src/traffic_analyzer.cpp
+++ traffic_analyze/src/traffic_analyzer.cpp
@@ -19,7 +19,7 @@
 #include <darknet_ros_msgs/BoundingBox.h>
 
 int vehicle_count_integer;
-int detection_id = 0;
+int detection_id = 1;
 // Signal-safe flag for whether shutdown is requested
 sig_atomic_t volatile g_request_shutdown = 0;
 
@@ -171,31 +171,32 @@ void count_object_no(const std_msgs::Int8::ConstPtr& count_value){
 }
 
 void extract_bounding_box(const darknet_ros_msgs::BoundingBoxes::ConstPtr& bbox){
-
-  // Negative coordinate error still persists....
-  const long int min_x = bbox->bounding_boxes[0].xmin;
-  const long int min_y = bbox->bounding_boxes[0].ymin;
-  const long int max_x = bbox->bounding_boxes[0].xmax;
-  const long int max_y = bbox->bounding_boxes[0].ymax;
-  std::string vehicle_class = bbox->bounding_boxes[0].Class;
-  const long int x_dimension_bbox = (max_x - min_x);
-  const long int y_dimension_bbox = (max_y - min_y);
-  const long int x_center = min_x + x_dimension_bbox/2;
-  const long int y_center = min_y + y_dimension_bbox/2;
   std::ofstream export_csv;
   export_csv.open("/home/master/catkin_ws/src/145P4P2019/csv/active_record.csv", std::ofstream::app);
-  export_csv << "Detection ID:," << detection_id <<"\n";
+  export_csv << "Frame ID:," << detection_id <<"\n";
   export_csv << "Count:," << vehicle_count_integer << "\n";
-  export_csv << "Vehicle Class:," << vehicle_class << "\n";
-  export_csv << "X-min:," << min_x << "\n";
-  export_csv << "Y-min:," << min_y << "\n";
-  export_csv << "X-max:," << max_x << "\n";
-  export_csv << "Y-max:," << max_y << "\n";
-  export_csv << "X-Length:," << x_dimension_bbox << "\n";
-  export_csv << "Y-Length:," << y_dimension_bbox << "\n";
-  export_csv << "X-Center:," << x_center << "\n";
-  export_csv << "Y-Center:," << y_center << "\n";
   export_csv.close();
+  for (int i = 0; i < bbox->bounding_boxes.size(); i++){
+    const long int min_x = bbox->bounding_boxes[i].xmin;
+    const long int min_y = bbox->bounding_boxes[i].ymin;
+    const long int max_x = bbox->bounding_boxes[i].xmax;
+    const long int max_y = bbox->bounding_boxes[i].ymax;
+    std::string vehicle_class = bbox->bounding_boxes[i].Class;
+    const long int x_dimension_bbox = (max_x - min_x);
+    const long int y_dimension_bbox = (max_y - min_y);
+    const long int x_center = min_x + x_dimension_bbox/2;
+    const long int y_center = min_y + y_dimension_bbox/2;
+    export_csv.open("/home/master/catkin_ws/src/145P4P2019/csv/active_record.csv", std::ofstream::app);
+    export_csv << "Vehicle Class:," << vehicle_class << "\n";
+    export_csv << "X-min:," << min_x << "\n";
+    export_csv << "Y-min:," << min_y << "\n";
+    export_csv << "X-max:," << max_x << "\n";
+    export_csv << "Y-max:," << max_y << "\n";
+    export_csv << "X-Length:," << x_dimension_bbox << "\n";
+    export_csv << "Y-Length:," << y_dimension_bbox << "\n";
+    export_csv << "X-Center:," << x_center << "\n";
+    export_csv << "Y-Center:," << y_center << "\n";
+    export_csv.close();
+  }
   detection_id++;
-
 }
--- traffic_tracker/src/traffic_tracker.cpp
+++ traffic_tracker/src/traffic_tracker.cpp
@@ -22,23 +22,24 @@ void extract_count(const std_msgs::Int8::ConstPtr& count_value);
 // global variables and prototypes
 cv::Mat frame;
 cv::Point center_point;
+long int frame_count = 0;
+long int bbox_count = 0;
+int bbox_no = 0;
 //std::vector<cv::Rect> ROIs;
-//std::vector<cv::Point> trajectory_points;
+std::vector<cv::Point> trajectory_points;
 
 int main(int arg, char **argv){
 
   ros::init(arg, argv, "traffic_tracker");
   ros::NodeHandle tt_nh;
-  ros::Rate loop_rate(10);
+  ros::Rate loop_rate(1000);
 
-  //cv::namedWindow("OpenCV_Feed");
-  //cv::startWindowThread();
   image_transport::ImageTransport transport_image(tt_nh);
   ros::Subscriber tt_bbox_sub = tt_nh.subscribe("/darknet_ros/bounding_boxes", 1000, extract_bbox);
-  ros::Subscriber tt_image_sub = tt_nh.subscribe("/darknet_ros/detection_image", 1000, extract_detection_image);
-  // debugging subscriber (subscribes to video_file.launch)
-  //ros::Subscriber tt_image_sub = tt_nh.subscribe("/videofile/image_raw", 1000, extract_detection_image);
-  //ros::Subscriber tt_count_sub = tt_nh.subscribe("/darknet_ros/found_object", 1000, extract_count);
+  //ros::Subscriber tt_image_sub = tt_nh.subscribe("/darknet_ros/detection_image", 1000, extract_detection_image);
+  //debugging subscriber (subscribes to video_file.launch)
+  ros::Subscriber tt_image_sub = tt_nh.subscribe("/videofile/image_raw", 1000, extract_detection_image);
+  ros::Subscriber tt_count_sub = tt_nh.subscribe("/darknet_ros/found_object", 1000, extract_count);
   while(ros::ok()){
     ros::spinOnce();
     loop_rate.sleep();
@@ -56,40 +57,38 @@ void extract_detection_image(const sensor_msgs::Image::ConstPtr& detection_image
     cv_bridge::CvImageConstPtr take_cv;
     take_cv = cv_bridge::toCvCopy(detection_image);
     frame = take_cv->image;
-    // for(size_t i = 0; i < trajectory_points.size(); i++){
-    //   cv::circle(frame, trajectory_points[i], 3, cv::Scalar(0,0,255), 2, 1);
-    // }
-    cv::circle(frame, center_point, 3, cv::Scalar(0,0,255), 2, 1);
-    cv::namedWindow("Tracker", CV_WINDOW_AUTOSIZE);
-    cv::imshow("Tracker", frame);
-    cv::waitKey(30);
-
+    for(size_t i = 0; i < trajectory_points.size(); i++){
+      cv::circle(frame, trajectory_points[i], 3, cv::Scalar(0,0,255), 2, 1);
     }
+  }
   catch(cv_bridge::Exception& e){
     ROS_ERROR("Error - cannot launch OpenCV", detection_image->encoding.c_str());
   }
+  cv::namedWindow("Tracker", CV_WINDOW_AUTOSIZE);
+  cv::imshow("Tracker", frame);
+  cv::waitKey(30);
 
 }
 
 void extract_bbox(const darknet_ros_msgs::BoundingBoxes::ConstPtr& bbox){
 
-  long int min_x = bbox->bounding_boxes[0].xmin;
-  long int min_y = bbox->bounding_boxes[0].ymin;
-  long int max_x = bbox->bounding_boxes[0].xmax;
-  long int max_y = bbox->bounding_boxes[0].ymax;
-  long int x_dimension_bbox = (max_x - min_x);
-  long int y_dimension_bbox = (max_y - min_y);
-  long int x_center = min_x + x_dimension_bbox/2;
-  long int y_center = min_y + y_dimension_bbox/2;
-  center_point = cv::Point(x_center, y_center);
-  //trajectory_points.push_back(center_point);
-
-  //ROIs.push_back(cv::Rect(min_x, min_y, x_dimension_bbox, y_dimension_bbox));
+  for(int i = 0; i < bbox->bounding_boxes.size(); i++){
+    long int min_x = bbox->bounding_boxes[i].xmin;
+    long int min_y = bbox->bounding_boxes[i].ymin;
+    long int max_x = bbox->bounding_boxes[i].xmax;
+    long int max_y = bbox->bounding_boxes[i].ymax;
+    long int x_dimension_bbox = (max_x - min_x);
+    long int y_dimension_bbox = (max_y - min_y);
+    long int x_center = min_x + x_dimension_bbox/2;
+    long int y_center = min_y + y_dimension_bbox/2;
+    center_point = cv::Point(x_center, y_center);
+    trajectory_points.push_back(center_point);
+  }
 
 }
 //
-// void extract_count(const std_msgs::Int8::ConstPtr& count_value){
-//
-//    bbox_no = count_value->data;
-//
-// }
+void extract_count(const std_msgs::Int8::ConstPtr& count_value){
+
+    bbox_no = count_value->data;
+
+}
